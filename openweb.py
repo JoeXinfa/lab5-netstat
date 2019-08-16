e42c74afd2553793edf5390ff2056231038042f9
Xinfa Joseph Zhu <xinfazhu@gmail.com>
2019-08-16 18:00:58 -0500

    Add the mode for Tor browser

--- openweb.py
+++ openweb.py
@@ -1,5 +1,5 @@
 # needed for Tor
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 # needed for other
 from selenium import webdriver
 
@@ -9,16 +9,21 @@ import subprocess as sub
 
 mode = sys.argv[1]
 
-if (mode == 'tor'):
+if mode == 'tor':
     # this is wherever your firefox script that Tor installs is
-    binary = '/home/seed/Downloads/tor-browser_en-US/Browser/firefox'
+    binary = '/home/joseph/Downloads/tor-browser-linux64-8.5.4_en-US/tor-browser_en-US/Browser/firefox'
     firefox_binary = FirefoxBinary(binary)
-    browser = webdriver.Firefox(firefox_binary=binary)
-elif (mode == 'firefox'):
+    #browser = webdriver.Firefox(firefox_binary=firefox_binary)
+elif mode == 'firefox':
     # this is wherever your geckodriver you downloaded is
     binary= '/usr/local/bin'
+    #browser = webdriver.Firefox(binary)
 else:
     raise NotImplementedError
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+cap = DesiredCapabilities().FIREFOX.copy()
+cap['marionette'] = False
 
 n = 10
 sites = [
@@ -37,7 +42,12 @@ for i in range(n):
     for site in sites:
         short, url = site
         print("------ work {} {}".format(i, url))
-        browser = webdriver.Firefox(binary)
+        # Open a new browser for each pcap
+        if mode == 'tor':
+            browser = webdriver.Firefox(firefox_binary=firefox_binary)
+            #browser = webdriver.Firefox(firefox_binary=firefox_binary, capabilities=cap)
+        if mode == 'firefox':
+            browser = webdriver.Firefox(binary)
         fn = "{}_{}_{}.pcap".format(mode, short, i)
         sub.Popen(['sudo', 'tcpdump', '-i', 'any', '-w', fn])
         browser.get(url)
